c78a1553a24ba75a285de89365aa5de37d490599
admin <itaebread@gmail.com>
2021-06-18 21:16:45 +0900

    210618

--- 24단계/01_1260.py
+++ 24단계/01_1260.py
@@ -1,2 +1,51 @@
-#bfs는 큐
-#dfs는 덱을 사용하자
+# import sys
+# N,M,V = map(int, sys.stdin.readline().split())
+# L = []
+# for i in range(M):
+#     L.append(list(map(int, sys.stdin.readline().split())))
+#
+# L.sort(key=lambda x:(x[0],x[1]))
+#
+#
+# now = V
+# traveled = 0
+# O = []
+# while traveled != N:
+#     tempL = []
+#     for i in range(M):
+#         if L[i][0] == now:
+#             tempL.append(L[i])
+#         print(tempL)
+#     tralveled += 1
+#     O.append
+
+N,M,V=map(int,input().split())
+chk=[[0]*(N+1) for i in range(N+1)]
+for i in range(M):
+    a,b = map(int,input().split())
+    chk[a][b]=chk[b][a]=1
+VL=[0]*(N+1)
+arr=[]
+arr2=[]
+def dfs(V):
+    VL[V]=1 
+    arr.append(V)
+    for i in range(1,N+1):
+        if(VL[i]==0 and chk[V][i]==1):
+            dfs(i)
+
+def bfs(V):
+    todo=[V]
+    VL[V]=0
+    while todo:
+        V=todo.pop(0)
+        arr2.append(V)
+        for i in range(1, N+1):
+            if(VL[i]==1 and chk[V][i]==1):
+                todo.append(i)
+                VL[i]=0
+
+dfs(V)
+bfs(V)
+print(*arr)
+print(*arr2)
--- 9단계/11_1002.py
+++ 9단계/11_1002.py
@@ -19,3 +19,26 @@
 #     r2 = inp[5]
 #
 # 쓰다보니 이렇게하는게 아닌거같음
+
+
+import math
+sqrt = math.sqrt
+n = int(input())
+
+for i in range(n):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    distance = sqrt((x1-x2)**2 + (y1-y2)**2)
+    #피타고라스의정리로 거리를 구했음
+    if distance == 0 and r1 == r2 :
+        # 두점이 겹쳐있다는 뜻
+        print(-1)
+    elif abs(r1-r2) == distance or r1 + r2 == distance:
+        #두점의 거리가 r1+r2와 같다면 가능한 위치는 하나이고 해당점을 기점으로 r1,r2를 반지름으로 하는 원을 그렸을때 외접함
+        #위처럼 원을 그렸을때 큰원의 반지름에서 두점 사이의 거리를 뺐을때 작은원의 반지름이 나온다면 내접하는 원임(가능한곳은 한개)
+        print(1)
+    elif abs(r1-r2) < distance < (r1+r2) :
+        #큰원반지름-작은원반지름 했는데 두점사이의 거리보단 작고 두점에서 목표위치까지의 거리를 합친것보다 작다면 두곳이가능함(벤다이어그램마냥 원이겹침)
+        print(2)
+    else:
+        print(0)
+        #ㅁㄹ 안만나나봄
