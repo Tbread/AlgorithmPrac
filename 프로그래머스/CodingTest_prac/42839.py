84bcb6620910943fa7db8fad3b8749828b311def
Tbread <81889764+Tbread@users.noreply.github.com>
2022-05-01 01:07:15 +0900

    20220501

--- 프로그래머스/CodingTest_prac/42839.py
+++ 프로그래머스/CodingTest_prac/42839.py
@@ -1,19 +1,33 @@
 import itertools,math
 root = math.sqrt
-comb = itertools.combinations
 def solution(numbers):
     numbers = list(map(int,list(numbers)))
     ea = len(numbers)
+    perms = []
+    for i in range(1,ea+1):
+        perms += list(itertools.permutations(numbers,i))
     combs = []
-    if ea == 1:
-        if numbers[0] == 2 or numbers[0] == 3 or numbers[0] == 1:
-            return 1
-        for i in range(2,int(root(numbers[0]))):
-            if root % i == 0:
-                return 0
-        return 1
-    for i in range(ea,1,-1):
-        combs.append(comb(numbers),ea)
-    print(combs)
-
-solution("017")
+    answer = 0
+    for i in range(len(perms)):
+        temp = ''
+        for j in range(len(perms[i])):
+            temp += str(perms[i][j])
+        combs.append(int(temp))
+    combs = list(set(combs))
+    for i in range(len(combs)):
+        flag = 0
+        if combs[i] <= 1:
+            continue
+        if combs[i] == 2:
+            answer += 1
+            continue
+        if combs[i] % 2 == 0:
+            continue
+        for j in range(2,int(root(combs[i]))+1):
+            if combs[i] % j == 0:
+                flag = 1
+                break
+        if flag == 0:
+            answer +=1
+    return answer
+solution("1231")
