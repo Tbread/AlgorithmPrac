9230524e6c191c5b825fbd69acb17c059497ea1b
Tbread <81889764+Tbread@users.noreply.github.com>
2022-05-02 01:21:54 +0900

    20220502

--- 백준/5430.py
+++ 백준/5430.py
@@ -6,27 +6,24 @@ for _ in range(repeat):
     commands = sys.stdin.readline().strip()
     flag = 0
     n = int(sys.stdin.readline())
-    temp_str = sys.stdin.readline().strip('[').strip().strip(']')
-    if temp_str:
-        deq = deque(map(int, temp_str.split(',')))
-    else:
-        deq = deque()
-    prev_commands = ''
-    while True:
-        if prev_commands == commands:
-            break
-        prev_commands = commands
-        commands = commands.replace('RR','')
-    for command in commands:
-        if command == 'D':
-            if not deq:
-                print('error')
-                flag = 1
-                break
-            else:
-                deq.popleft()
-        else:
-            deq.reverse()
-    if flag == 1:
+    temp_str = deque(eval(sys.stdin.readline().strip()))
+    commands = commands.replace('RR', '')
+    isRev = 0
+    if commands.count('D') > n:
+        print('error')
         continue
-    print(deq)
+    for command in commands:
+        if command == 'R':
+            if isRev == 1:
+                isRev = 0
+            else:
+                isRev = 1
+        elif command == 'D':
+            if isRev == 1:
+                temp_str.pop()
+            else:
+                temp_str.popleft()
+    if isRev == 1:
+        print(str(list(reversed(temp_str))).replace(' ',''))
+    else:
+        print(str(list(temp_str)).replace(' ',''))
