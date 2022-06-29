1f586696111fea9f6a2e1eac88ed3ccce59b8ed6
Tbread <81889764+Tbread@users.noreply.github.com>
2022-06-29 19:33:34 +0900

    20220629

--- 프로그래머스/CodingTest_prac/43238.py
+++ 프로그래머스/CodingTest_prac/43238.py
@@ -1,6 +1,20 @@
 def solution(n, times):
-    headCount = len(times)
-    aver = n // headCount
+    answer = float("inf")
+    min = 0
+    max = sorted(times)[-1] * n
+    while (min <= max):
+        mid = (min + max) // 2
+        cnt = 0
+        for i in times:
+            cnt += mid // i
+        if cnt < n:
+            min = mid + 1
+        else:
+            if mid < answer:
+                answer = mid
+            max = mid - 1
+    return answer
 
 
-solution(6,[7,10])
+
+solution(6, [7, 10])
